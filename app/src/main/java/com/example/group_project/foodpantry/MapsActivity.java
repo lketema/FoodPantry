07007920d154dcba509df7c81f8838e769b37a83
lketema <lketema@terpmail.umd.edu>
2018-11-24 18:23:20 -0500

    added location permission and requests to get permission, ignored the error message for sdk.

--- app/src/main/java/com/example/group_project/foodpantry/MapsActivity.java
+++ app/src/main/java/com/example/group_project/foodpantry/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.example.group_project.foodpantry;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,10 +15,15 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private final int LOCATION_PERMISSION_CODE = 1;
+    private String TAG = "Maps Activity";
+    private Boolean useZipCode = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,5 +52,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void getPhoneLocation(){
+        Log.i(TAG, "Getting Phone's Location");
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted  --- request perms, return handled by onRequestPermissionsResult
+            requestPermissions(new String[] {ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+            return;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case LOCATION_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getPhoneLocation();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Location Access Not Granted", Toast.LENGTH_LONG).show();
+                    Log.i(TAG, "Location permission was not granted");
+                    useZipCode = true;
+                }
+                return;
+            }
+
+        }
+
     }
 }
