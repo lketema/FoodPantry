3806b3a7362eee6fac22a0240b781be41e989513
lketema <lketema@terpmail.umd.edu>
2018-11-24 19:36:07 -0500

    added user location to map, still needs testing.

--- app/src/main/java/com/example/group_project/foodpantry/MapsActivity.java
+++ app/src/main/java/com/example/group_project/foodpantry/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.example.group_project.foodpantry;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -8,12 +10,16 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
@@ -21,9 +27,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Location mCurrentLocation;
     private final int LOCATION_PERMISSION_CODE = 1;
     private String TAG = "Maps Activity";
     private Boolean useZipCode = false;
+    private Boolean permissionGranted = false;
+    private Boolean gotLocation = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,29 +41,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //get last known location through fused location
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if(gotLocation){
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude())));
+        }
+       // LatLng sydney = new LatLng(-34, 151);
+       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     private void getPhoneLocation(){
+        getPhoneLocationPermission();
+        try{
+        if(permissionGranted){
+            Task lastLocation = mFusedLocationProviderClient.getLastLocation();
+            lastLocation.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){ //got location
+                        gotLocation = true;
+                        mCurrentLocation = (Location) task.getResult();
+                    }
+                    else{
+                       Log.i(TAG, "Could not get location");
+                    }
+                }
+            });
+        }
+        else{
+            //TODO implement use by zipcode
+        } }
+        catch (SecurityException e){
+            // needs to handles or error
+            Log.i(TAG, "Security Exception when getting locaton: " + e.getMessage());
+        }
+    }
+
+    private void getPhoneLocationPermission(){
         Log.i(TAG, "Getting Phone's Location");
         if (ActivityCompat.checkSelfPermission(MapsActivity.this, ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -62,7 +93,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             requestPermissions(new String[] {ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
             return;
         }
-
+        else{
+           //permission granted for location
+            permissionGranted = true;
+        }
     }
 
     @Override
@@ -71,7 +105,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             case LOCATION_PERMISSION_CODE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    getPhoneLocation();
+                    getPhoneLocationPermission();
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "Location Access Not Granted", Toast.LENGTH_LONG).show();
