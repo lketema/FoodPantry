d8ede44913134b045c9048c3b3b2a2212cc74037
Hunter <hunterk0212@gmail.com>
2018-12-01 11:44:26 -0500

    fixing code so shah can push

--- app/src/main/java/com/example/group_project/foodpantry/Recipient.java
+++
@@ -1,8 +0,0 @@
-package com.example.group_project.foodpantry;
-
-public class Recipient extends User {
-
-    public Recipient(String name, String email, String password) {
-        super(name, email, password);
-    }
-}
--- app/src/main/java/com/example/group_project/foodpantry/Volunteer.java
+++
@@ -1,9 +0,0 @@
-package com.example.group_project.foodpantry;
-
-public class Volunteer extends User {
-
-    public Volunteer(String name, String email, String password) {
-        super(name, email, password);
-    }
-
-}
