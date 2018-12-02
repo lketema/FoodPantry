1fb8bf47d5f0a7c8147efa908aee00291a14ae25
devinrowell <devrow97@gmail.com>
2018-12-02 15:45:43 -0500

    added new back button functionality

--- app/src/main/java/com/example/group_project/foodpantry/EventConfirmationActivity.java
+++ app/src/main/java/com/example/group_project/foodpantry/EventConfirmationActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.content.Intent;
 
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -85,5 +86,15 @@ public class EventConfirmationActivity extends AppCompatActivity {
 
     private boolean hasWebsite() {
         return !website.equals("");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(EventConfirmationActivity.this,
+                OptionsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+
     }
 }
